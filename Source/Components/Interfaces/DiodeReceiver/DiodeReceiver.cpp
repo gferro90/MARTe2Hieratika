6d4bc9c093dc05c1ab7b5c76a92e86661190b25a
Giuseppe Ferro <frrgpp01@uniroma2.it>
2020-02-18 17:49:53 +0100

    3

--- Source/Components/Interfaces/DiodeReceiver/DiodeReceiver.cpp
+++ Source/Components/Interfaces/DiodeReceiver/DiodeReceiver.cpp
@@ -864,7 +864,7 @@ bool DiodeReceiver::ReadVarNameAndIndex(StreamString &payload,
                     found = (StringHelper::CompareN(payload.Buffer() + i, pattern, patternSize) == 0);
                     if (!found) {
                         //cannot find a " in the name... resync
-                        REPORT_ERROR(ErrorManagement::Information, "The var name cannot contain \": resync %d %d\n", i, currentSize);
+                        //REPORT_ERROR(ErrorManagement::Information, "The var name cannot contain \": resync %d %d\n", i, currentSize);
                         payload.Seek(0ull);
                         MemoryOperationsHelper::Copy((void*) payload.Buffer(), payload.Buffer() + i, currentSize);
                         payload.SetSize(currentSize);
