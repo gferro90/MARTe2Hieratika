441b26744d093356eef758d3a0820e16119dbb27
Giuseppe Ferro <frrgpp01@uniroma2.it>
2020-02-19 18:26:11 +0100

    8

--- Source/Components/Interfaces/EpicsParserAndSubscriber/EpicsParserAndSubscriber.cpp
+++ Source/Components/Interfaces/EpicsParserAndSubscriber/EpicsParserAndSubscriber.cpp
@@ -587,9 +587,9 @@ void EpicsParserAndSubscriber::CleanContext(uint32 threadId) {
             for (uint32 n = beg; (n < end); n++) {
                 REPORT_ERROR(ErrorManagement::Information, "Cazz 2");
 
-                (void) ca_clear_subscription(pvDescriptor[n].pvEvid);
-                (void) ca_clear_event(pvDescriptor[n].pvEvid);
-                (void) ca_clear_channel(pvDescriptor[n].pvChid);
+                //(void) ca_clear_subscription(pvDescriptor[n].pvEvid);
+                //(void) ca_clear_event(pvDescriptor[n].pvEvid);
+                //(void) ca_clear_channel(pvDescriptor[n].pvChid);
             }
             //ca_detach_context();
             //ca_context_destroy();
