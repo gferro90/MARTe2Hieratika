05bf58e4b2e4c7f6758abee88c98a93765e70634
Giuseppe Ferro <frrgpp01@uniroma2.it>
2020-02-19 17:58:11 +0100

    6

--- Source/Components/Interfaces/EpicsParserAndSubscriber/EpicsParserAndSubscriber.cpp
+++ Source/Components/Interfaces/EpicsParserAndSubscriber/EpicsParserAndSubscriber.cpp
@@ -242,7 +242,7 @@ bool EpicsParserAndSubscriber::Initialise(StructuredDataI &data) {
                 maxArraySize = MAX_ARR_LEN;
             }
 
-            uint8 numberofCpus;
+            uint8 numberOfCpus;
             ret = data.Read("NumberOfCpus", numberOfCpus);
             if (!ret) {
                 REPORT_ERROR(ErrorManagement::InitialisationError, "Please specify the NumberOfCpus");
