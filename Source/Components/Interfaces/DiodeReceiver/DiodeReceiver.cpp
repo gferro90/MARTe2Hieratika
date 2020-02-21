38bf9bad40c6ab19da236ea9046ceea0413b5740
Giuseppe Ferro <frrgpp01@uniroma2.it>
2020-02-21 09:07:16 +0100

    fdsfsdf

--- Source/Components/Interfaces/DiodeReceiver/DiodeReceiver.cpp
+++ Source/Components/Interfaces/DiodeReceiver/DiodeReceiver.cpp
@@ -797,7 +797,7 @@ ErrorManagement::ErrorType DiodeReceiver::ReadNewChunk(TCPSocket * const commCli
         else {
             chunkSize = contentLength;
         }
-        REPORT_ERROR(ErrorManagement::Information, "Chunk size = %d", chunkSize);
+        //REPORT_ERROR(ErrorManagement::Information, "Chunk size = %d", chunkSize);
 
         /*if (chunkSize > 1023u) {
             //REPORT_ERROR(ErrorManagement::Information, "Chunk size = %d", chunkSize);
