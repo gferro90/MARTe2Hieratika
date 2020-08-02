db2e76d672092277650c5066c5223ec936043de6
Giuseppe Ferro <frrgpp01@uniroma2.it>
2020-08-02 19:00:40 +0200

    mah

--- Source/Components/Interfaces/PrioritySender/PrioritySender.cpp
+++ Source/Components/Interfaces/PrioritySender/PrioritySender.cpp
@@ -191,7 +191,7 @@ void PrioritySenderCycleLoop(PrioritySender &arg) {
             //wait the cycle time
             uint32 elapsed = (uint32)((float32)((HighResolutionTimer::Counter() - arg.lastTickCounter) * 1000u * HighResolutionTimer::Period()));
 
-            printf("elapsed=%d\n", elapsed);
+            printf("elapsed=%d ticks=%d\n", elapsed, (HighResolutionTimer::Counter() - arg.lastTickCounter));
             if (elapsed < arg.msecPeriod) {
                 Sleep::MSec(arg.msecPeriod - elapsed);
             }
