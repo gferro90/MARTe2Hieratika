b78beac8220c686d3c2a1d5c434abdf2bf093d6f
Giuseppe Ferro <frrgpp01@uniroma2.it>
2020-11-21 22:45:01 +0100

    removed logger. Debugging

--- Source/Components/Interfaces/PrioritySender/PrioritySender.cpp
+++ Source/Components/Interfaces/PrioritySender/PrioritySender.cpp
@@ -405,7 +405,7 @@ bool PrioritySender::SetDataSource(EpicsParserAndSubscriber &dataSourceIn) {
         changeFlag = (uint8*) HeapManager::Malloc(numberOfVariables);
         reconnectionCycleCounter = new uint32*[numberOfPoolThreads];
         destinationsMask = new uint8[numberOfPoolThreads];
-        packetsNotAck = new uint8[numberOfPoolThreads];
+        packetsNotAck = new uint32[numberOfPoolThreads];
         tickAfterPost = new uint64[2 * numberOfPoolThreads];
         for (uint32 i = 0u; i < numberOfPoolThreads; i++) {
             reconnectionCycleCounter[i] = new uint32[numberOfDestinations];
